c9916cefb40e1b27b4630190fb9020ed41a10002
ShonaOB <shona-obriendev@outlook.com>
2022-03-19 19:15:53 +0000

    add image data to javascript file and test using console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,24 @@ const section = document.querySelector('section');
 const scoreCount = document.querySelector('span');
 let lives = 8;
 scoreCount.textContent = lives;
+
+
+//gathering the data together
+const getData = () => [
+    {imgSrc: "images/ariel.jpg", name:'ariel'},
+    {imgSrc: "images/aurora.jpg", name:'aurora'},
+    {imgSrc: "images/belle.jpg", name:'belle'},
+    {imgSrc: "images/jasmine.jpg", name:'jasmine'},
+    {imgSrc: "images/merida.jpg", name:'merida'},
+    {imgSrc: "images/mulan.jpg", name:'mulan'},
+    {imgSrc: "images/pocahontas.jpg", name:'pocahontas'},
+    {imgSrc: "images/snowwhite.jpg", name:'snowwhite'},
+    {imgSrc: "images/ariel.jpg", name:'ariel'},
+    {imgSrc: "images/aurora.jpg", name:'aurora'},
+    {imgSrc: "images/belle.jpg", name:'belle'},
+    {imgSrc: "images/jasmine.jpg", name:'jasmine'},
+    {imgSrc: "images/merida.jpg", name:'merida'},
+    {imgSrc: "images/mulan.jpg", name:'mulan'},
+    {imgSrc: "images/pocahontas.jpg", name:'pocahontas'},
+    {imgSrc: "images/snowwhite.jpg", name:'snowwhite'},
+]
